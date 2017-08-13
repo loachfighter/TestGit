e94ba9eb3e77efc349a3bfdbb40e53144ab0ebe5
zylloach <zylloach@foxmail.com>
2017-08-13 23:01:29 +0800

    first commit

---
+++ src/HelloGit.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Loachfighter on 2017/8/13.
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("HelloGit!");
+    }
+}
