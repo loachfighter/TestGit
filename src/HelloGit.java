c455d8deb70db04c280c7274f0908fab4bb46aeb
linboo <zylloach@foxmail.com>
2020-03-25 17:30:38 +0800

    测试Git.1.5

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -4,6 +4,6 @@
 public class HelloGit {
     public static void main(String[] args) {
         // write your code here
-        System.out.println("HelloGit!   ee1221");
+        System.out.println("HelloGit!   ee1999221");
     }
 }
